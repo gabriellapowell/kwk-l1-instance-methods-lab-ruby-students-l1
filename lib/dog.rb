b66c5de79252979c232385aa308ccd076c6d2f8a
gabriellapowell <soccerplayer60@icloud.com>
2018-06-08 19:32:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 # dog.rb
 class Dog 
-  def instance (bark, sit)
-   @bark = bark
-   @sit = sit
-  end
+  # def instance (bark, sit)
+  # @bark = bark
+  # @sit = sit
+  # end
 
   def bark
-   @bark = bark
+  # @bark = bark
   # bark = "Woof!"
     puts "Woof!"
   end
 
   def sit
-   @sit = sit
+  # @sit = sit
    puts "The Dog is sitting"
   end
 
