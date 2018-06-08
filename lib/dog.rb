b047a86c40926031bf8af0c852e40f7a969b48f2
gabriellapowell <soccerplayer60@icloud.com>
2018-06-08 19:33:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # dog.rb
+class Dog 
+  # def instance (bark, sit)
+  # @bark = bark
+  # @sit = sit
+  # end
+
+  def bark
+  # @bark = bark
+  # bark = "Woof!"
+    puts "Woof!"
+  end
+
+  def sit
+  # @sit = sit
+   puts "The Dog is sitting"
+  end
+
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # person.rb
+class Person 
+#   def instance(talk, walk)
+#   @talk
+#   @walk
+# end
+
+  def talk
+    # @talk = talk
+    talk = "Hello World!"
+    puts talk
+  end
+  
+  def walk
+    # @walk = walk
+    puts "The Person is walking"
+  end
+end
+
