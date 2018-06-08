0ac8419b347f54df88e5c629557a1aa243ba4d18
gabriellapowell <soccerplayer60@icloud.com>
2018-06-08 19:32:57 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 # person.rb
 class Person 
-  def instance(talk, walk)
-  @talk
-  @walk
-end
+#   def instance(talk, walk)
+#   @talk
+#   @walk
+# end
 
   def talk
-    @talk = talk
+    # @talk = talk
     talk = "Hello World!"
     puts talk
   end
   
   def walk
-    @walk = walk
+    # @walk = walk
     puts "The Person is walking"
   end
 end
